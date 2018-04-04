beb2a4468ec2dfc4df1f0f26db898cb661bcaecf
Yipin Zhu <d1251@labpc03.lss.uni-stuttgart.de>
2018-04-04 17:27:35 +0200

    4/4

--- libs/util.py
+++ libs/util.py
@@ -15,12 +15,12 @@ from matplotlib import gridspec
 dictChName = {'fat':'F',
               'water':'W'}
 
-def findCh_dir(channel, lst_dir):
+def findCh_dir(channel, lst_dir):               #lst_dir is lstChDirNames: KoRA/KORA123456/study/1_vibe_dixon_F5      list
     """Find channel direction."""
-    n = int(channel[-1])
-    name = channel[:-1]
-    ch_dir = list(filter(lambda dir_name: dir_name[-4]==dictChName[name], lst_dir))[n]
-    return ch_dir
+    n = int(channel[-1])   # n = 0 or 1
+    name = channel[:-1]     # name = fat or water
+    ch_dir = list(filter(lambda dir_name: dir_name[-4]==dictChName[name], lst_dir))[n]          #filter out the names of files in lst_dir whose 4th last num is "F" or "W"
+    return ch_dir                                       #ch_dir is KoRA/KORA123456/study/1_vibe_dixon_F56
  
 def del0002(lst_dcm):
     """Delete list's "0002"-entries."""
@@ -51,19 +51,19 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
         # get reference for patient:
         lstChDirNames = os.listdir(PathDicom)
         lstChDirNames.sort()
-        PathRef = os.path.join(PathDicom,lstChDirNames[1])
-        DirRef = del0002(os.listdir(PathRef))
+        PathRef = os.path.join(PathDicom,lstChDirNames[1])      #lstChDirNames: KoRA/KORA123456/study/1_vibe_dixon_F5     list
+        DirRef = del0002(os.listdir(PathRef))                      #PATHREF: KORA/KORA1223454/STUDY/T1_vibe_dixon_F56/
 
         brk = False
         try: 
-            PathRef = os.path.join(PathRef,DirRef[0])
+            PathRef = os.path.join(PathRef,DirRef[0])           #pathref = KoRA/KORA123456/study/T1_vibe_dixon_F56/
         except IndexError:
             if verbose:
                 print("Invalid reference, skipping patient " + patient + ".")
             brk = True
         if brk: continue
     
-        RefDs = dicom.read_file(PathRef)
+        RefDs = dicom.read_file(PathRef)   #this is only read out for loading dims
         # load dimensions:
         ConstPixelDims = (int(RefDs.Rows),
                           int(RefDs.Columns),
@@ -75,33 +75,34 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
                              float(RefDs.SliceThickness))
    
         ArrayDicom = np.zeros(ConstPixelDims, dtype='float16')
-        DicomPathsList = [0]*channels
+        DicomPathsList = [0]*channels           #channels = 3
         # walk through channels:
         for ch_index,ch in enumerate(lst_ch):
             try:
-                ch_dir = findCh_dir(ch, lstChDirNames)
+                ch_dir = findCh_dir(ch, lstChDirNames)          #ch_dir is 1_vibe_dixon_F56
             except IndexError:
                 if verbose:
                     print("Channel does not exits, skipping patient " + patient + ".")
                 brk = True
                 break 
-            ch_path = os.path.join(PathDicom,ch_dir)
-            DicomPathsList[ch_index] = ch_path
+            ch_path = os.path.join(PathDicom,ch_dir)            #pathDicom = KoRA/KORA123456/study/
+            DicomPathsList[ch_index] = ch_path                  #ch_path = KoRA/KORA123456/study/1_vibe_dixon_F_56       DicomPathsList[0] = KoRA/KORA123456/study/1_vibe_dixon_F56
+                                                                # DicomPathsList[1] = KoRA/KORA123456/study/1_vibe_dixon_W_56
             lstFilesDCM = del0002(os.listdir(ch_path))
             # avoid double entries:
             lstFilesDCM = list(set(lstFilesDCM))
             # sort in ascending order:
-            lstFilesDCM.sort()
-            lstPathsDCM = list(map(lambda file_name: os.path.join(ch_path, file_name), lstFilesDCM))
+            lstFilesDCM.sort()                                  #lstFilesDCM = KoRA/KORA123456/study/1_vibe_dixon_F56/asb.dcm    list without same name and sorted
+            lstPathsDCM = list(map(lambda file_name: os.path.join(ch_path, file_name), lstFilesDCM))     # for each file in lstFilesDCM, join path KoRA/KORA123456/study/1_vibe_dixon_F56/asb.dcm   list
             if verbose: print("Loading " + ch_path + "...")
             # loop through all the DICOM files
-            for z, pathDCMch in enumerate(lstPathsDCM):
+            for z, pathDCMch in enumerate(lstPathsDCM):         #this read out all the files in the lstFilesDCM = KoRA/KORA123456/study/1_vibe_dixon_F56
                 # read the files
                 ds = dicom.read_file(pathDCMch)
                 # store the raw image data
                 brk = False
                 try:
-                    ArrayDicom[:, :, z, ch_index] = ds.pixel_array.T
+                    ArrayDicom[:, :, z, ch_index] = ds.pixel_array.T    # add each file as a slice in the 3d image, (in each channel)
                 except NotImplementedError:
                     # decompress dicom file
                     decomp_filename = '/tmp/curr_patient_decomp.dcm'
@@ -125,7 +126,7 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
         if brk: continue
 
         try:
-            lbl_switch = (os.listdir(PathNii)[-1][:6] == 'label_')
+            lbl_switch = (os.listdir(PathNii)[-1][:6] == 'label_')      #PathNii = KoRA/KORA123456/study/xxx_ROI/....  find the last one and see if it start with label_
         except OSError:
                 PathNii = os.path.join(path, patient, patient + "_ROi")
                 try: 
@@ -143,10 +144,10 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
         NiiPathsList = [0]*(classes-1)
         # walk through classes/labels:
         for cl_index, cl in enumerate(lst_cl):
-            label_path = os.path.join(PathNii, lbl_switch * 'label_' + cl + '2d.nii')
+            label_path = os.path.join(PathNii, lbl_switch * 'label_' + cl + '2d.nii')       #lbl_switch is a boolean boolean * 'label_' means start with label or not
             brk = False            
             try:
-                label = nib.load(label_path)
+                label = nib.load(label_path)            # this need some change because the new label is in h5py
             except Exception:
                 if verbose:
                     print("No label, skipping patient " + patient + ".")
@@ -159,7 +160,7 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
                     print("Invalid label-dimension, skipping patient " + patient + '.')
                 brk = True
                 break
-            NiiPathsList[cl_index] = label_path
+            NiiPathsList[cl_index] = label_path                   #NiiPathsList[0] = liver2d.nii NiiPathsList[1] = spleen2d.nii
     
         if ArrayNii.max() >= classes:
             if verbose: print("Ambiguous labeling, skipping patient " + patient + '.')
@@ -174,7 +175,7 @@ def check_patients(path, lst_ch, lst_cl, path_save=None, verbose=False):
     # save paths on hard disk with pickle
     if path_save is None:
         with open(os.path.join(path,'patients.pkl'), 'wb') as dicoms:
-            pickle.dump(lstDicomPaths, dicoms)
+            pickle.dump(lstDicomPaths, dicoms)                      #lstDicomPaths is a list of DicomPathsList(contain all channels path of all patients in turn)
         with open(os.path.join(path,'labels.pkl'), 'wb') as niis:
             pickle.dump(lstNiiPaths, niis)
     else: 
@@ -216,7 +217,7 @@ class Patient():
     def get_slices(self, count=True, verbose=False):
         """Take loaded slices if already available or load slices if not."""
         if count:        
-            self.slice_counter += 1
+            self.slice_counter += 1                 #this slices counter actually stands for the num of patients
         if self.slices is None:
             self.slices = self.load_slices(verbose=verbose)
             #print("slice-counter: " + str(self.slice_counter))
@@ -253,7 +254,7 @@ class Patient():
 
         # get reference for patient:
         DirRef = del0002(os.listdir(self.dicom_dirs[0]))
-        PathRef = os.path.join(self.dicom_dirs[0],DirRef[0])
+        PathRef = os.path.join(self.dicom_dirs[0],DirRef[0])     #dicom_dirs = train_path_dcm = (a path in) train_paths_dcm = DicomPathsList[cut:] = pickle.load('patients.pkl')
         RefDs = dicom.read_file(PathRef)
         # Load dimensions:
         ConstPixelDims = (int(RefDs.Rows),
@@ -528,8 +529,8 @@ def split(k, i, lst, perc):
     ys = [lst[j] for j in idxs]
     result = []
     for y in ys:
-        result += y[0:int(len(y)*perc)]             #every time add a half of all lst elements, exlucde the current validation fold ?????
-    return (lst[i], result)
+        result += y[0:int(len(y)*perc)]
+    return (lst[i], result)    
            
 def load_correct_patients(path, 
                           patients_to_take, 
@@ -542,7 +543,7 @@ def load_correct_patients(path,
                           verbose=False):
     """Load valid patient-paths from hard disk and generate patient-objects."""
     with open(os.path.join(path,'patients.pkl'), 'rb') as patients:
-        DicomPathsList = pickle.load(patients)                      #what is included in the patient.pkl, what is included in the DicomPathList???
+        DicomPathsList = pickle.load(patients)
     with open(os.path.join(path,'labels.pkl'), 'rb') as labels:
         NiiPathsList = pickle.load(labels)
 
@@ -574,7 +575,7 @@ def load_correct_patients(path,
     if last_val_patients is not None:
         for patient in last_val_patients:
             #print(' drop validation patients')
-            patient.drop()          #what does this means to drop validation patient, drop from buffer?
+            patient.drop()
 
     # generate patient-objects
     patients_test  = []
@@ -588,13 +589,13 @@ def load_correct_patients(path,
         patient = Patient(val_path_dcm, val_path_nii, forget_slices=forget_slices)
         patients_val.append(patient)
     for train_path_dcm, train_path_nii in zip(train_paths_dcm, train_paths_nii):
-        patient = Patient(train_path_dcm, train_path_nii, forget_slices=forget_slices)
+        patient = Patient(train_path_dcm, train_path_nii, forget_slices=forget_slices)          #train_path_dicom = (a path in) train_paths_dcm = DicomPathsList[cut:] = pickle.load('patients.pkl')
         patients_train.append(patient)
         
     # get slices for validation data
     patients_val_slices = []
     for patient in patients_val:
-        slices = patient.get_slices(verbose=verbose)                    #what are those slices used for?????
+        slices = patient.get_slices(verbose=verbose)
         patients_val_slices.append(slices)
 
     return (patients_test, patients_train, patients_val, patients_val_slices)
